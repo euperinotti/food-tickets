3e8c8f3b30757eea947d1f5c79dfdd3dc1f8547e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-26 12:53:40 -0300

    chore: create base project structure

---
+++ api/src/test/java/br/com/euperinotti/food_tickets/FoodTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.euperinotti.food_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
