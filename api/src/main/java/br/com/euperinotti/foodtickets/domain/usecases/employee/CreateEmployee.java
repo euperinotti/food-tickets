d72ca4b0820b40b4be8080f8a042e6106e87fe0f
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 19:04:39 -0300

    feat: add employee controller

--- api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/employee/CreateEmployee.java
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/employee/CreateEmployee.java
@@ -3,6 +3,7 @@ package br.com.euperinotti.foodtickets.domain.usecases.employee;
 import br.com.euperinotti.foodtickets.domain.dtos.request.EmployeeRequestDTO;
 import br.com.euperinotti.foodtickets.domain.dtos.response.EmployeeResponseDTO;
 import br.com.euperinotti.foodtickets.domain.entities.EmployeeBO;
+import br.com.euperinotti.foodtickets.domain.enums.EmployeeStatus;
 import br.com.euperinotti.foodtickets.domain.mappers.EmployeeMapper;
 import br.com.euperinotti.foodtickets.domain.repository.IEmployeeRepository;
 
@@ -15,6 +16,8 @@ public class CreateEmployee {
   }
 
   public EmployeeResponseDTO execute(EmployeeRequestDTO dto) {
+    dto.setStatus(EmployeeStatus.ACTIVE);
+
     EmployeeBO bo = EmployeeMapper.toBO(dto);
     EmployeeBO created = repository.save(bo);
 
---
+++ api/src/main/java/br/com/euperinotti/foodtickets/presentation/controllers/EmployeeController.java
@@ -0,0 +1,62 @@
+package br.com.euperinotti.foodtickets.presentation.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.euperinotti.foodtickets.domain.dtos.request.EmployeeRequestDTO;
+import br.com.euperinotti.foodtickets.domain.dtos.response.EmployeeResponseDTO;
+import br.com.euperinotti.foodtickets.services.EmployeeService;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+  private final EmployeeService employeeService;
+
+  @Autowired
+  public EmployeeController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<EmployeeResponseDTO>> getAllEmployees() {
+    List<EmployeeResponseDTO> employees = employeeService.getAll();
+    return ResponseEntity.ok(employees);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<EmployeeResponseDTO> getEmployeeById(@PathVariable Long id) {
+    EmployeeResponseDTO employee = employeeService.getById(id);
+
+    return ResponseEntity.ok(employee);
+  }
+
+  @GetMapping("/status/{status}")
+  public ResponseEntity<List<EmployeeResponseDTO>> getEmployeesByStatus(@PathVariable String status) {
+    List<EmployeeResponseDTO> employees = employeeService.getByStatus(status);
+    return ResponseEntity.ok(employees);
+  }
+
+  @PostMapping
+  public ResponseEntity<EmployeeResponseDTO> createEmployee(@RequestBody EmployeeRequestDTO dto) {
+    EmployeeResponseDTO createdEmployee = employeeService.create(dto);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdEmployee);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<EmployeeResponseDTO> updateEmployee(@PathVariable Long id,
+      @RequestBody EmployeeRequestDTO dto) {
+    EmployeeResponseDTO updatedEmployee = employeeService.update(id, dto);
+    return ResponseEntity.ok(updatedEmployee);
+  }
+}
