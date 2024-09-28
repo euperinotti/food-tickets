d04519372ec4cbe26c3fe923fa1274704fdd7299
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 07:33:28 -0300

    refactor: create employee implementation

--- api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/employee/CreateEmployee.java
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/employee/CreateEmployee.java
@@ -1,6 +1,9 @@
 package br.com.euperinotti.foodtickets.domain.usecases.employee;
 
+import br.com.euperinotti.foodtickets.domain.dtos.request.EmployeeRequestDTO;
+import br.com.euperinotti.foodtickets.domain.dtos.response.EmployeeResponseDTO;
 import br.com.euperinotti.foodtickets.domain.entities.EmployeeBO;
+import br.com.euperinotti.foodtickets.domain.mappers.EmployeeMapper;
 import br.com.euperinotti.foodtickets.domain.repository.IEmployeeRepository;
 
 public class CreateEmployee {
@@ -11,9 +14,11 @@ public class CreateEmployee {
     this.repository = repository;
   }
 
-  public EmployeeBO execute(EmployeeBO bo) {
+  public EmployeeResponseDTO execute(EmployeeRequestDTO dto) {
+    EmployeeBO bo = EmployeeMapper.toBO(dto);
     EmployeeBO created = repository.save(bo);
-    return created;
+
+    return EmployeeMapper.toResponseDTO(created);
   }
 
 }
