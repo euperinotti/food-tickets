ed6209667821f67d85408d19f7f50e75e4f36a9e
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 19:32:23 -0300

    feat: add ticket mapper

---
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/mappers/TicketMapper.java
@@ -0,0 +1,28 @@
+package br.com.euperinotti.foodtickets.domain.mappers;
+
+import br.com.euperinotti.foodtickets.domain.dtos.request.TicketRequestDTO;
+import br.com.euperinotti.foodtickets.domain.dtos.response.TicketResponseDTO;
+import br.com.euperinotti.foodtickets.domain.entities.TicketBO;
+
+public class TicketMapper {
+  public static TicketBO toBO(TicketRequestDTO dto) {
+
+    TicketBO bo = new TicketBO(dto.getId(), dto.getEmployeeId(), dto.getStatus(), dto.getCreatedAt(),
+        dto.getUpdatedAt());
+
+    return bo;
+  }
+
+  public static TicketResponseDTO toResponseDTO(TicketBO bo) {
+    TicketResponseDTO dto = new TicketResponseDTO();
+
+    dto.setId(bo.getId());
+    dto.setEmployeeId(bo.getEmployeeId());
+    dto.setStatus(bo.getStatus());
+    dto.setCreatedAt(bo.getCreatedAt());
+    dto.setUpdatedAt(bo.getUpdatedAt());
+
+    return dto;
+  }
+
+}
