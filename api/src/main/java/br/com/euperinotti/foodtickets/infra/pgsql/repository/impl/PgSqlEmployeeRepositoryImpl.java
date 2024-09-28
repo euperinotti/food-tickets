abed96e68641d375b756efd2c9417d5e8ad2cbe2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 19:06:32 -0300

    refactor: add component annotation to repository impl

--- api/src/main/java/br/com/euperinotti/foodtickets/infra/pgsql/repository/impl/PgSqlEmployeeRepositoryImpl.java
+++ api/src/main/java/br/com/euperinotti/foodtickets/infra/pgsql/repository/impl/PgSqlEmployeeRepositoryImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import br.com.euperinotti.foodtickets.domain.entities.EmployeeBO;
@@ -13,6 +14,7 @@ import br.com.euperinotti.foodtickets.infra.pgsql.entities.PgSqlEmployeeEntity;
 import br.com.euperinotti.foodtickets.infra.pgsql.mappers.PgSqlEmployeeMapper;
 import br.com.euperinotti.foodtickets.infra.pgsql.repository.contracts.PgSqlEmployeeRepository;
 
+@Component("pgSqlEmployeeRepositoryImpl")
 @Repository
 public class PgSqlEmployeeRepositoryImpl implements IEmployeeRepository {
 
