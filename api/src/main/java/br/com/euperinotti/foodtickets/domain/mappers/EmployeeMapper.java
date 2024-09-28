7a05ba8d35ac855b65c198ddb66b9633c52b1fae
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 07:32:49 -0300

    feat: add implementations to employee mapper

--- api/src/main/java/br/com/euperinotti/foodtickets/domain/mappers/EmployeeMapper.java
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/mappers/EmployeeMapper.java
@@ -1,4 +1,27 @@
 package br.com.euperinotti.foodtickets.domain.mappers;
 
+import br.com.euperinotti.foodtickets.domain.dtos.request.EmployeeRequestDTO;
+import br.com.euperinotti.foodtickets.domain.dtos.response.EmployeeResponseDTO;
+import br.com.euperinotti.foodtickets.domain.entities.EmployeeBO;
+
 public class EmployeeMapper {
+  public static EmployeeBO toBO(EmployeeRequestDTO dto) {
+    EmployeeBO bo = new EmployeeBO(dto.getId(), dto.getName(), dto.getCpf(), dto.getStatus(), dto.getCreatedAt(),
+        dto.getUpdatedAt());
+
+    return bo;
+  }
+
+  public static EmployeeResponseDTO toResponseDTO(EmployeeBO bo) {
+    EmployeeResponseDTO dto = new EmployeeResponseDTO();
+
+    dto.setId(bo.getId());
+    dto.setName(bo.getName());
+    dto.setCpf(bo.getCpf());
+    dto.setStatus(bo.getStatus());
+    dto.setCreatedAt(bo.getCreatedAt());
+    dto.setUpdatedAt(bo.getUpdatedAt());
+
+    return dto;
+  }
 }
