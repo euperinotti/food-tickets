a8109dd2219a69ae9f02616cc55a6417b1aa8e8c
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 16:37:54 -0300

    test: fix test cases

--- api/src/test/java/br/com/euperinotti/foodtickets/domain/mappers/EmployeeMapperTest.java
+++ api/src/test/java/br/com/euperinotti/foodtickets/domain/mappers/EmployeeMapperTest.java
@@ -15,7 +15,6 @@ public class EmployeeMapperTest {
 
   @Test
   public void test_toBO_successfulMapping() {
-    // Arrange
     EmployeeRequestDTO dto = new EmployeeRequestDTO();
     dto.setId(1L);
     dto.setName("John Doe");
@@ -24,13 +23,11 @@ public class EmployeeMapperTest {
     dto.setCreatedAt(LocalDate.now());
     dto.setUpdatedAt(LocalDate.now());
 
-    // Act
     EmployeeBO bo = EmployeeMapper.toBO(dto);
 
-    // Assert
     assertEquals(dto.getId(), bo.getId());
-    assertEquals(dto.getName(), bo.getName());
-    assertEquals(dto.getCpf(), bo.getCpf());
+    assertEquals("JOHN DOE", bo.getName());
+    assertEquals("12345678910", bo.getCpf());
     assertEquals(dto.getStatus(), bo.getStatus());
     assertEquals(dto.getCreatedAt(), bo.getCreatedAt());
     assertEquals(dto.getUpdatedAt(), bo.getUpdatedAt());
@@ -38,17 +35,14 @@ public class EmployeeMapperTest {
 
   @Test
   public void test_toResponseDTO_successfulMapping() {
-    // Arrange
     EmployeeBO bo = new EmployeeBO(1L, "John Doe", "123.456.789-10", EmployeeStatus.ACTIVE, LocalDate.now(),
         LocalDate.now());
 
-    // Act
     EmployeeResponseDTO dto = EmployeeMapper.toResponseDTO(bo);
 
-    // Assert
     assertEquals(bo.getId(), dto.getId());
-    assertEquals(bo.getName(), dto.getName());
-    assertEquals(bo.getCpf(), dto.getCpf());
+    assertEquals("JOHN DOE", dto.getName());
+    assertEquals("12345678910", dto.getCpf());
     assertEquals(bo.getStatus(), dto.getStatus());
     assertEquals(bo.getCreatedAt(), dto.getCreatedAt());
     assertEquals(bo.getUpdatedAt(), dto.getUpdatedAt());
--- api/src/test/java/br/com/euperinotti/foodtickets/infra/pgsql/mappers/PgSqlEmployeeMapperTest.java
+++ api/src/test/java/br/com/euperinotti/foodtickets/infra/pgsql/mappers/PgSqlEmployeeMapperTest.java
@@ -14,17 +14,14 @@ public class PgSqlEmployeeMapperTest {
 
   @Test
   public void test_toEntity_successfulMapping() {
-    // Arrange
     EmployeeBO bo = new EmployeeBO(1L, "John Doe", "123.456.789-10", EmployeeStatus.ACTIVE, LocalDate.now(),
         LocalDate.now());
 
-    // Act
     PgSqlEmployeeEntity entity = PgSqlEmployeeMapper.toEntity(bo);
 
-    // Assert
     assertEquals(bo.getId(), entity.getId());
-    assertEquals(bo.getName(), entity.getName());
-    assertEquals(bo.getCpf(), entity.getCpf());
+    assertEquals("JOHN DOE", entity.getName());
+    assertEquals("12345678910", entity.getCpf());
     assertEquals(bo.getStatus(), entity.getStatus());
     assertEquals(bo.getCreatedAt(), entity.getCreatedAt());
     assertEquals(bo.getUpdatedAt(), entity.getUpdatedAt());
@@ -32,7 +29,6 @@ public class PgSqlEmployeeMapperTest {
 
   @Test
   public void test_toBO_successfulMapping() {
-    // Arrange
     PgSqlEmployeeEntity entity = new PgSqlEmployeeEntity();
     entity.setId(1L);
     entity.setName("John Doe");
@@ -41,13 +37,11 @@ public class PgSqlEmployeeMapperTest {
     entity.setCreatedAt(LocalDate.now());
     entity.setUpdatedAt(LocalDate.now());
 
-    // Act
     EmployeeBO bo = PgSqlEmployeeMapper.toBO(entity);
 
-    // Assert
     assertEquals(entity.getId(), bo.getId());
-    assertEquals(entity.getName(), bo.getName());
-    assertEquals(entity.getCpf(), bo.getCpf());
+    assertEquals("JOHN DOE", bo.getName());
+    assertEquals("12345678910", bo.getCpf());
     assertEquals(entity.getStatus(), bo.getStatus());
     assertEquals(entity.getCreatedAt(), bo.getCreatedAt());
     assertEquals(entity.getUpdatedAt(), bo.getUpdatedAt());
