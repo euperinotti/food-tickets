61802c55509c3ca06deaf0089366377bb2e180f4
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 07:25:03 -0300

    feat: add employee exceptions

---
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/exceptions/enums/EmployeeExceptions.java
@@ -0,0 +1,19 @@
+package br.com.euperinotti.foodtickets.domain.exceptions.enums;
+
+public enum EmployeeExceptions {
+  EMPLOYEE_NOT_FOUND("Employee not found"),
+  EMPLOYEE_INVALID_STATUS("Invalid employee status"),
+  EMPLOYEE_INVALID_NAME("Invalid employee name"),
+  EMPLOYEE_INVALID_EMAIL("Invalid employee email"),
+  EMPLOYEE_INVALID_PHONE("Invalid employee phone");
+
+  private String message;
+
+  EmployeeExceptions(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
