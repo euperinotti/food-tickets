1ffbe5b72d46864b7b2bba2b97389afd941a946c
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-28 19:33:08 -0300

    feat: add ticket repository interface

---
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/repository/ITicketRepository.java
@@ -0,0 +1,21 @@
+package br.com.euperinotti.foodtickets.domain.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import br.com.euperinotti.foodtickets.domain.entities.TicketBO;
+import br.com.euperinotti.foodtickets.domain.enums.TicketStatus;
+
+public interface ITicketRepository {
+  TicketBO save(TicketBO Ticket);
+
+  Optional<TicketBO> findById(Long id);
+
+  void deleteById(Long id);
+
+  List<TicketBO> findAll();
+
+  List<TicketBO> findByStatus(TicketStatus status);
+
+  TicketBO updateById(Long id, TicketBO Ticket);
+}
