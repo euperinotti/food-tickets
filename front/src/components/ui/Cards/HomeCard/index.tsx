2d07a5622b35981093e0ee8aa6e7abff27abdb17
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-29 12:23:42 -0300

    feat: card home component

---
+++ front/src/components/ui/Cards/HomeCard/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+
+export interface HomeCardProps {
+  title: string;
+  description: string;
+}
+
+export const HomeCard = ({ title, description }: HomeCardProps) => {
+  return (
+    <div className="flex w-[300px] rounded-lg border border-gray-200 bg-white flex-col max-w-sm shadow-none disabled:hover gap-2">
+      <div className="flex w-full h-full flex-col justify-center gap-2 p-6">
+        <span className="text-sm tracking-tight text-gray-600">{title}</span>
+        <h5 className="text-3xl font-bold text-gray-900">{description}</h5>
+      </div>
+    </div>
+  );
+};
