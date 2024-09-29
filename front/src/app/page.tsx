3c2a7f13b038a96ad21a6b1edf8756238afca058
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-29 12:24:28 -0300

    refactor: home page

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,20 @@
-import { BaseTemplate } from "@/template/Base";
+import { FlowbiteSidebar } from "@/components/flowbite/Sidebar";
+import { HomeCard } from "@/components/ui/Cards/HomeCard";
 
 export default function Home() {
   return (
-    <BaseTemplate>
-      <h1>Hello World</h1>
-    </BaseTemplate>
+    <div className="flex justify-center items-center w-full h-dvh">
+      <FlowbiteSidebar />
+      <main className="p-8 flex flex-1 flex-col h-full">
+        <div className="flex flex-col gap-4 w-full">
+          <h1 className="text-4xl font-semibold w-full">Dashboard</h1>
+          <div className="flex w-full items-center justify-start gap-4">
+            <HomeCard title="Tickets entregues" description="1237" />
+            <HomeCard title="Funcionários ativos" description="600" />
+            <HomeCard title="Período com mais tickets entregues" description="12/08/2024" />
+          </div>
+        </div>
+      </main>
+    </div>
   );
 }
