1d789105206d008ea44402bcf0a46ae22f34eb0d
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-09-29 23:27:13 -0300

    feat: add inputs components

---
+++ front/src/components/ui/Input/InputText/index.tsx
@@ -0,0 +1,11 @@
+import { GenericInputProps } from "../types";
+
+export const InputText = ({ ...rest }: GenericInputProps) => {
+  return (
+    <input
+      type="text"
+      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      {...rest}
+    />
+  );
+};
