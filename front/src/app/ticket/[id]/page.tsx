a1401c8b8b3f49f7c20f0fc2a696cfb8e0f126c6
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-01 02:18:57 -0300

    feat: add tickets page

---
+++ front/src/app/ticket/[id]/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+import { ITicket } from "@/@types/ITicket";
+import { API_PROVIDER } from "@/axios/api";
+import { HeadlessCombobox } from "@/components/headless/Combobox";
+import { Button } from "@/components/ui/Button";
+import { InputContainer } from "@/components/ui/Input/InputContainer";
+import { InputNumber } from "@/components/ui/Input/InputNumber";
+import { InputRadio } from "@/components/ui/Input/InputRadio";
+import Sidebar from "@/components/ui/Sidebar";
+import { FormEvent, useEffect, useState } from "react";
+
+const tableData = [
+  {
+    id: 1,
+    name: "John Doe",
+    cpf: "123.456.789-00",
+    status: "A",
+    createdAt: "2024-12-29 09:51:35",
+    updatedAt: "2024-12-29 09:51:35",
+  },
+  {
+    id: 1,
+    name: "Kevin Hart",
+    cpf: "123.456.789-00",
+    status: "I",
+    createdAt: "2024-12-29 09:51:35",
+    updatedAt: "2024-12-29 09:51:35",
+  },
+];
+
+const fetchData = async (id: string) => {
+  // const response = await API_PROVIDER.getEmployeeById(id);
+  // if (!response.ok) {
+  //   throw new Error("Falha ao buscar os dados");
+  // }
+
+  return {
+    id: 1,
+    employeeId: 10,
+    status: "A",
+    createdAt: "2024-12-29 09:51:35",
+    updatedAt: "2024-12-29 09:51:35",
+  };
+};
+
+const initialState: ITicket = {
+  id: null,
+  employeeId: 10,
+  status: "A",
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString(),
+};
+
+export default function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const [form, setForm] = useState<ITicket>(initialState);
+
+  useEffect(() => {
+    (async () => {
+      if (id !== "new") {
+        const response = await fetchData(id);
+
+        setForm(response);
+      }
+    })();
+  }, [id]);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (form && form.id) {
+      API_PROVIDER.updateTicket({ ...form });
+    } else {
+      API_PROVIDER.createTicket({ ...form });
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center w-full h-dvh">
+      <Sidebar />
+      <main className="p-8 flex flex-1 flex-col h-full gap-12">
+        <div className="flex gap-4 w-full justify-between">
+          <h1 className="text-4xl font-semibold">Ticket</h1>
+        </div>
+        <div className="h-full w-full overflow-y-scroll">
+          <form
+            className="bg-white w-[600px] h-auto rounded-md overflow-hidden"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col item-start justify-start gap-4 p-5">
+              <InputContainer label="Funcionário">
+                <HeadlessCombobox
+                  displayKey="name"
+                  filterKey="name"
+                  data={tableData}
+                />
+              </InputContainer>
+              <InputContainer label="CPF">
+                <InputNumber />
+              </InputContainer>
+              <InputContainer label="Status">
+                <InputRadio
+                  label="Ativo"
+                  name="employee-status"
+                  value="A"
+                  id="employee-active"
+                  defaultChecked={form.status === "A"}
+                  onClick={(e) => setForm({ ...form, status: "A" })}
+                />
+                <InputRadio
+                  label="Inativo"
+                  name="employee-status"
+                  value="I"
+                  id="employee-inactive"
+                  defaultChecked={form.status === "I"}
+                  onClick={(e) => setForm({ ...form, status: "I" })}
+                />
+              </InputContainer>
+            </div>
+            <div className="flex item-start justify-end gap-4 p-5">
+              <Button
+                label="Limpar"
+                styleType="danger"
+                type="reset"
+                onClick={() => setForm(initialState)}
+              />
+              <Button label="Salvar" styleType="default" type="submit" />
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ front/src/app/tickets/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { Button } from "@/components/ui/Button";
+import Sidebar from "@/components/ui/Sidebar";
+import { Table } from "@/components/ui/Table";
+import { useRouter } from "next/navigation";
+
+const tableColumns = [
+  { title: "Nome", key: "name" },
+];
+
+const tableData = [
+  {
+    id: 1,
+    employeeId: 10,
+    status: "A",
+    createdAt: "2024-12-29 09:51:35",
+    updatedAt: "2024-12-29 09:51:35",
+  },
+];
+
+export default function Index() {
+  const router = useRouter();
+
+  return (
+    <div className="flex justify-center items-center w-full h-dvh">
+      <Sidebar />
+      <main className="p-8 flex flex-1 flex-col h-full gap-12">
+        <div className="flex gap-4 w-full justify-between">
+          <h1 className="text-4xl font-semibold">Tickets</h1>
+          <Button
+            label={"Novo ticket"}
+            styleType="default"
+            onClick={() => router.push("/ticket/new")}
+          />
+        </div>
+        <div className="max-h-fit w-full">
+          <Table
+            columns={tableColumns}
+            data={tableData}
+            onEditRow={(e) => {
+              router.push(`/ticket/${e.id}`);
+            }}
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
