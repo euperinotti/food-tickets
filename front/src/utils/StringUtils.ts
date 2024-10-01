00029ad07759063fdefd7ed208786020630bf0c2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-01 01:48:53 -0300

    feat: add string utils

---
+++ front/src/utils/StringUtils.ts
@@ -0,0 +1,10 @@
+export const StringUtils = {
+  formatCpf(value: string) {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+      .slice(0, 14);
+  },
+};
