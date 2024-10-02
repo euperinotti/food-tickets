e36dee993b178074a1f15b7e13ca67f427335252
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-01 21:01:10 -0300

    refactor: create ticket use case

--- api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/ticket/CreateTicket.java
+++ api/src/main/java/br/com/euperinotti/foodtickets/domain/usecases/ticket/CreateTicket.java
@@ -1,27 +1,45 @@
 package br.com.euperinotti.foodtickets.domain.usecases.ticket;
 
+import java.time.LocalDate;
+
 import br.com.euperinotti.foodtickets.domain.dtos.request.TicketRequestDTO;
 import br.com.euperinotti.foodtickets.domain.dtos.response.TicketResponseDTO;
+import br.com.euperinotti.foodtickets.domain.entities.EmployeeBO;
 import br.com.euperinotti.foodtickets.domain.entities.TicketBO;
 import br.com.euperinotti.foodtickets.domain.enums.TicketStatus;
+import br.com.euperinotti.foodtickets.domain.exceptions.AppExceptions;
+import br.com.euperinotti.foodtickets.domain.exceptions.enums.EmployeeExceptions;
 import br.com.euperinotti.foodtickets.domain.mappers.TicketMapper;
+import br.com.euperinotti.foodtickets.domain.repository.IEmployeeRepository;
 import br.com.euperinotti.foodtickets.domain.repository.ITicketRepository;
 
 public class CreateTicket {
 
   private ITicketRepository repository;
+  private IEmployeeRepository employeeRepository;
 
-  public CreateTicket(ITicketRepository repository) {
+  public CreateTicket(ITicketRepository repository, IEmployeeRepository employeeRepository) {
     this.repository = repository;
+    this.employeeRepository = employeeRepository;
   }
 
   public TicketResponseDTO execute(TicketRequestDTO dto) {
-    dto.setStatus(TicketStatus.ACTIVE);
+    validate(dto);
 
     TicketBO bo = TicketMapper.toBO(dto);
     TicketBO created = repository.save(bo);
 
     return TicketMapper.toResponseDTO(created);
+  }
+
+  public void validate(TicketRequestDTO dto) {
+    EmployeeBO employee = employeeRepository.findById(dto.getEmployeeId())
+        .orElseThrow(() -> new AppExceptions(EmployeeExceptions.EMPLOYEE_NOT_FOUND.getMessage()));
+
+    dto.setId(employee.getId());
+    dto.setStatus(TicketStatus.ACTIVE);
+    dto.setCreatedAt(LocalDate.now());
+    dto.setUpdatedAt(LocalDate.now());
   }
 
 }
