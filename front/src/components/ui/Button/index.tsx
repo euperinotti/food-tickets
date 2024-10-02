919fc3ea86bbdfff6c67d2fb47fe2a137d6f3b67
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-02 20:30:03 -0300

    refactor: add new button style

--- front/src/components/ui/Button/index.tsx
+++ front/src/components/ui/Button/index.tsx
@@ -4,6 +4,7 @@ export const ButtonType = {
   default:
     "bg-blue-700 border border-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-800 active:bg-blue-600",
   danger: "bg-gray-100 border border-gray-200 text-red-600 py-2 px-6 rounded-md hover:bg-gray-200 active:bg-gray-300",
+  outline: "border border-blue-600 text-blue-600 py-2 px-6 rounded-md"
 };
 
 export const Button = ({
