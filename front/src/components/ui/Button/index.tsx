6e48806e0fef80c5a01ebd43599d663de1056ef2
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 01:05:47 -0300

    styles: component styles

--- front/src/components/ui/Button/index.tsx
+++ front/src/components/ui/Button/index.tsx
@@ -2,7 +2,7 @@ import { ButtonProps } from "./types";
 
 export const ButtonType = {
   default:
-    "bg-blue-700 border border-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-800 active:bg-blue-600",
+    "bg-blue-600 border border-blue-600 text-white py-2 px-6 rounded-md hover:bg-blue-700 active:bg-blue-600",
   danger: "bg-gray-100 border border-gray-200 text-red-600 py-2 px-6 rounded-md hover:bg-gray-200 active:bg-gray-300",
   outline: "border border-blue-600 text-blue-600 py-2 px-6 rounded-md"
 };
