64406868905d59f3eaa3755d8b385b3d40f3674a
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 13:35:46 -0300

    refactor: next config file

---
+++ front/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
+  images: {
+    formats: ['image/webp']
+  },
+  poweredByHeader: false
+};
