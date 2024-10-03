ff94f1e721b080d3a2d0c0f68b942a500e748aee
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 01:04:26 -0300

    feat: add select component

---
+++ front/src/components/ui/Select/index.tsx
@@ -0,0 +1,30 @@
+import { SelectHTMLAttributes } from "react";
+
+export interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement> {
+  options: {
+    key: string;
+    label: string;
+    placeholder: string;
+    data: any[];
+  };
+}
+
+export const Select = ({ options, ...rest }: SelectProps) => {
+  return (
+    <select
+      {...rest}
+      className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-1 focus:ring-blue-500"
+    >
+      <option value="">{options.placeholder}</option>
+      {options.data.map((item) => (
+        <option
+          key={item[options.key]}
+          value={item[options.key]}
+          selected={rest.value == item[options.key]}
+        >
+          {item.name}
+        </option>
+      ))}
+    </select>
+  );
+};
