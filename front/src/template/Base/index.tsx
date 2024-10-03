e13d59869a775c7dbde15d68c01b2ce00f86e29c
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 19:55:38 -0300

    refactor: base template component

--- front/src/template/Base/index.tsx
+++ front/src/template/Base/index.tsx
@@ -1,16 +1,11 @@
+import Sidebar from "@/components/ui/Sidebar";
 import { BaseTemplateProps } from "./types";
 
-export const BaseTemplate = ({ children, ...rest }: BaseTemplateProps) => {
+export const BaseTemplate = ({ children }: BaseTemplateProps) => {
   return (
-    <div
-      className={`bg-blue-900 min-h-dvh h-full text-slate-50 py-12 px-60 ${rest.className}`}
-      {...rest}
-    >
-      <div className="flex flex-col justify-start items-start h-full w-full gap-12">
-        <div className="flex flex-col justify-start items-start h-auto w-full gap-6">
-          {children}
-        </div>
-      </div>
+    <div className="flex justify-center items-center w-full h-dvh">
+      <Sidebar />
+      <main className="p-8 flex flex-1 flex-col h-full">{children}</main>
     </div>
   );
 };
