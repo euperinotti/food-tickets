62c6ba3cad41a4c0e92bceeabca814b37bb16c57
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 20:55:44 -0300

    refactor: toast config

--- front/src/hooks/useAlert.ts
+++ front/src/hooks/useAlert.ts
@@ -1,18 +1,26 @@
 "use client";
 import { ToastStatus } from "@/@types/Toast";
-import { toast } from "react-toastify";
+import { toast, ToastOptions } from "react-toastify";
 
 export const useAlert = () => {
+  const toastConfig: ToastOptions = {
+    position: "bottom-right",
+    autoClose: 3500,
+    closeOnClick: true,
+    draggable: false,
+    containerId: 1,
+  };
+
   const notify = (status: ToastStatus, message: string) => {
     switch (status) {
       case ToastStatus.SUCCESS:
-        toast.success(message);
+        toast.success(message, toastConfig);
         break;
       case ToastStatus.ERROR:
-        toast.error(message);
+        toast.error(message, toastConfig);
         break;
       default:
-        toast.info(message);
+        toast.info(message, toastConfig);
         break;
     }
   };
