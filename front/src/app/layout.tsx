a141c8861ac3abee97c46ffdecd8c3dc9328191c
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 08:22:06 -0300

    refactor: implementing toast context

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ToastProvider } from "@/context/ToastContext";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -28,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh h-full`}
       >
-        {children}
+        <ToastProvider>{children}</ToastProvider>
       </body>
     </html>
   );
