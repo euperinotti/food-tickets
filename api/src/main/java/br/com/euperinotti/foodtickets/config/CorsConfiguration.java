90984622c019de5d4a9f22d45f8ac005278b52ca
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-03 23:41:37 -0300

    chore: add cors configuration

---
+++ api/src/main/java/br/com/euperinotti/foodtickets/config/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package br.com.euperinotti.foodtickets.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(@NonNull CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+  }
+}
