ba56933bf1caeb37c628bc1ac5b66b04821e2cba
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-04 11:32:11 -0300

    test: cpf validator tests

--- api/src/test/java/br/com/euperinotti/foodtickets/domain/validators/CpfValidatorTest.java
+++ api/src/test/java/br/com/euperinotti/foodtickets/domain/validators/CpfValidatorTest.java
@@ -2,71 +2,91 @@ package br.com.euperinotti.foodtickets.domain.validators;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
 class CpfValidatorTest {
 
-  @Test
-  void test_isValidCPF_shouldReturnTrueForValidCPF() {
-    String validCpf = "12345678909";
+    @Test
+    void test_isValidCPF_shouldReturnTrueForValidCPF() {
+        String validCpf = "12345678909";
 
-    boolean result = CpfValidator.isValidCPF(validCpf);
+        boolean result = CpfValidator.isValidCPF(validCpf);
 
-    assertTrue(result, "Deveria retornar true para um CPF válido");
-  }
+        assertTrue(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForInvalidCPF() {
-    String invalidCpf = "12345678900";
+    @Test
+    void test_isValidCPF_shouldReturnFalseForInvalidCPF() {
+        String invalidCpf = "12345678900";
 
-    boolean result = CpfValidator.isValidCPF(invalidCpf);
+        boolean result = CpfValidator.isValidCPF(invalidCpf);
 
-    assertFalse(result, "Deveria retornar false para um CPF inválido");
-  }
+        assertFalse(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForCPFWithRepeatedNumbers() {
-    String repeatedCpf = "11111111111";
+    @Test
+    void test_isValidCPF_shouldReturnFalseForCPFWithRepeatedNumbers() {
+        String repeatedCpf = "11111111111";
 
-    boolean result = CpfValidator.isValidCPF(repeatedCpf);
+        boolean result = CpfValidator.isValidCPF(repeatedCpf);
 
-    assertFalse(result, "Deveria retornar false para CPFs com números repetidos");
-  }
+        assertFalse(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForEmptyCPF() {
-    String emptyCpf = "";
+    @Test
+    void test_isValidCPF_shouldReturnFalseForEmptyCPF() {
+        String emptyCpf = "";
 
-    boolean result = CpfValidator.isValidCPF(emptyCpf);
+        boolean result = CpfValidator.isValidCPF(emptyCpf);
 
-    assertFalse(result, "Deveria retornar false para CPF vazio");
-  }
+        assertFalse(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForNullCPF() {
-    String nullCpf = null;
+    @Test
+    void test_isValidCPF_shouldReturnFalseForNullCPF() {
+        String nullCpf = null;
 
-    boolean result = CpfValidator.isValidCPF(nullCpf);
+        boolean result = CpfValidator.isValidCPF(nullCpf);
 
-    assertFalse(result, "Deveria retornar false para CPF nulo");
-  }
+        assertFalse(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForCpfWithLessThan11Digits() {
-    String shortCpf = "12345678";
+    @Test
+    void test_isValidCPF_shouldReturnFalseForCpfWithLessThan11Digits() {
+        String shortCpf = "12345678";
 
-    boolean result = CpfValidator.isValidCPF(shortCpf);
+        boolean result = CpfValidator.isValidCPF(shortCpf);
 
-    assertFalse(result, "Deveria retornar false para CPF com menos de 11 dígitos");
-  }
+        assertFalse(result);
+    }
 
-  @Test
-  void test_isValidCPF_shouldReturnFalseForCpfWithMoreThan11Digits() {
-    String longCpf = "123456789012";
+    @Test
+    void test_isValidCPF_shouldReturnFalseForCpfWithMoreThan11Digits() {
+        String longCpf = "123456789012";
 
-    boolean result = CpfValidator.isValidCPF(longCpf);
+        boolean result = CpfValidator.isValidCPF(longCpf);
 
-    assertFalse(result, "Deveria retornar false para CPF com mais de 11 dígitos");
-  }
+        assertFalse(result);
+    }
+
+    @Test
+    void test_isValidCPF_shouldReturnFalseForRepeatedNumbers() {
+        String[] repeatedCpfs = {
+            "00000000000",
+            "11111111111",
+            "22222222222",
+            "33333333333",
+            "44444444444",
+            "55555555555",
+            "66666666666",
+            "77777777777",
+            "88888888888",
+            "99999999999"
+        };
+
+        for (String cpf : repeatedCpfs) {
+            boolean result = CpfValidator.isValidCPF(cpf);
+            assertFalse(result);
+        }
+    }
 }
