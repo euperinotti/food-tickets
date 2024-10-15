f49690bf44962832f9ed6332240852ff634b3656
Guilherme Perinotti <guilherme9115@hotmail.com>
2024-10-15 11:59:43 -0300

    refactor: home card component

--- front/src/components/ui/Cards/HomeCard/index.tsx
+++ front/src/components/ui/Cards/HomeCard/index.tsx
@@ -4,18 +4,18 @@ import { HomeCardProps } from "./types";
 export const HomeCard = ({ title, description, icon, ...rest }: HomeCardProps) => {
   return (
     <Card>
-                <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-                  <CardTitle className='text-sm font-medium'>
-                    {title}
-                  </CardTitle>
-                  {icon}
-                </CardHeader>
-                <CardContent>
-                  <div className='text-2xl font-bold'>{description}</div>
-                  <p className='text-xs text-muted-foreground'>
-                    +20.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
+      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+        <CardTitle className='text-base font-normal'>
+          {title}
+        </CardTitle>
+        {icon}
+      </CardHeader>
+      <CardContent>
+        <div className='text-2xl font-bold'>{description}</div>
+        <p className='text-xs text-muted-foreground'>
+          +20.1% from last month
+        </p>
+      </CardContent>
+    </Card>
   );
 };
--- front/src/components/ui/Cards/HomeCard/types.ts
+++ front/src/components/ui/Cards/HomeCard/types.ts
@@ -3,5 +3,5 @@ import { HTMLAttributes, ReactNode } from "react";
 export interface HomeCardProps extends HTMLAttributes<HTMLDivElement> {
   title: string;
   description: string | number;
-  icon: ReactNode
+  icon?: ReactNode
 }
